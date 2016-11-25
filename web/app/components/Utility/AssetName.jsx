9b3bdd927e7aa7bbb8ac1e0568d77c9707c8fc70
Sigve Kvalsvik <sigvekvalsvik@gmail.com>
2016-11-25 15:59:48 +0100

    Remove assetname tooltip

--- web/app/components/Utility/AssetName.jsx
+++ web/app/components/Utility/AssetName.jsx
@@ -38,15 +38,7 @@ class AssetName extends React.Component {
 			let desc = asset_utils.parseDescription(asset.getIn(["options", "description"]));
 			let tooltip = `<div><strong>${this.props.name}</strong><br />${desc.short ? desc.short : desc.main}</div>`;
 			return (
-				<span
-					className="tooltip"
-					data-tip={tooltip}
-					data-place="bottom"
-					data-type="light"
-					data-html={true}
-				>
-					<span className="asset-prefix-replaced">{prefix}</span><span>{replacedName}</span>
-				</span>
+				<span><span className="asset-prefix-replaced">{prefix}</span><span>{replacedName}</span></span>
 			);
 		} else {
 			return <span>{prefix}<span>{name}</span></span>
